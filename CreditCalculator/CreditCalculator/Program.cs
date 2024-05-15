43d0ee7a958b8a6062da37d657916139e723a0c7
Kirill <kirill.0312@mail.ru>
2024-05-15 23:28:06 +0300

    Кредитный калькулятор

---
+++ CreditCalculator/CreditCalculator/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Credit calculator!");
+
+// X = c * k
+// k коэффициент ануниента
+// с сумма кредита
+// x ежемесячный платеж
+// k = m *(1+m)^s/((1+m)^s -1)
+// m - месячная процентная ставка (если кредит под 24 годовых, то m = 2)
+// s - количество пратежей = кол-во месяцев если платим каждый месяц
+// 
+
+Console.WriteLine("какая сумма кредита?");
+
+var c = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("какая процентная ставка?");
+
+var personPeyYear = double.Parse(Console.ReadLine()!) / 100; // преобразуем проценты в нормальную форму
+
+Console.WriteLine("какая месяцев выплаты кредита?");
+
+var s = int.Parse(Console.ReadLine()!);
+
+var m = personPeyYear / 12;
+// коэффициент ануниента
+var k = m * Math.Pow(1 + m, s) / (Math.Pow(1 + m, s) - 1);
+// ежемесячный платеж
+var x = c * k;
+
+var payment = Math.Round(x, 2);
+var totalPaymant = x * s;
+var totalPercent = totalPaymant - c;
+
+var mainDeptInPercent = (c / totalPaymant) * 100;
+var percentsInPercent = 100 - mainDeptInPercent;
+
+Console.WriteLine($"ежемесячный платеж: {payment:C}");
+Console.WriteLine($"всего платежей: {totalPaymant:C}");
+Console.WriteLine($"начисленные проценты: {totalPercent:C}");
+Console.WriteLine($"От общей уплаченой суммы. основной долг: {mainDeptInPercent:0.00}%; кол-во переплаченых процентов: {percentsInPercent:0.00}%");
+
+
+var dept = c;
+
+var paymentNumber = 0;
+var sumPayment = 0d;
+var sumMaindebtPerson = 0d;
+var sumPersonPerson = 0d;
+
+double sumPercentPayment = 0d;
+double sumMainDeptPayment = 0d;
+while (dept > 0)
+{
+    
+    paymentNumber++;
+    var percentPayment = dept * m;
+    var mainDeptPayment = payment - percentPayment;
+
+    if (dept < mainDeptPayment)
+    {
+        mainDeptPayment = dept;
+        percentPayment = 0;
+        payment = mainDeptPayment;
+    }
+
+
+    dept -= mainDeptPayment;
+
+    var info = new PaymentInfo(paymentNumber, payment, Math.Round(mainDeptPayment, 2), Math.Round(percentPayment));
+    Console.WriteLine(info);
+
+    sumPayment += payment;
+    sumMainDeptPayment += mainDeptPayment;
+    sumPercentPayment += percentPayment;
+}
+
+Console.WriteLine($"Выплечено всего: {sumPayment:C}");
+Console.WriteLine($"Сумма выплначенного долга: {sumMainDeptPayment:C}");
+Console.WriteLine($"Сумма выплаченных процетов: {sumPercentPayment:C}");
+
+Console.ReadLine();
+
+record PaymentInfo(int PaymentNumber, double Payment, double MainDeptPayment, double Percent);
