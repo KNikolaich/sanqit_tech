8b729e0577c89c744c8a521f24801f6b15cbf2d1
Kirill <kirill.0312@mail.ru>
2024-05-22 17:56:36 +0300

    Part2

---
+++ credit-calculator-part-2/CreditCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using CreditCalculator;
+using CreditCalculator.Extensions;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Credit calculator");
+
+Console.WriteLine("Какая сумма кредита?");
+var c = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Какая процентная ставка?");
+var percentPerYear = double.Parse(Console.ReadLine()!) / 100;
+Console.WriteLine("На сколько месяцев берёте креди?");
+var s = int.Parse(Console.ReadLine()!);
+
+var calculator = new Calculator();
+
+var calculationResult = calculator.Calculate(new CalculationParameters(
+    c,
+    percentPerYear,
+    s
+));
+
+calculationResult.Print();
+
+Console.ReadKey();
