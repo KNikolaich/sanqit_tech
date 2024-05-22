d93a1dcde3115bf84b76b769132ea136b9d824f3
Kirill <kirill.0312@mail.ru>
2024-05-22 17:35:31 +0300

    калькулятор 0.1

---
+++ CreditCalculator/CreditCalculator/CreditCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace CreditCalculator;
+
+internal class CreditCalculator
+{
+    public CalculationResult Calc(
+        CalculationParameters parameters)
+    {
+
+        var m = parameters.personPeyYear / 12;
+        // коэффициент ануниента
+        var k = m * Math.Pow(1 + m, parameters.periods) / (Math.Pow(1 + m, parameters.periods) - 1);
+        // ежемесячный платеж
+        var x = parameters.summOfCredit * k;
+
+        var payment = x;
+        var totalPayment = x * parameters.periods;
+        var totalPercent = totalPayment - parameters.summOfCredit;
+
+        var mainDeptInPercent = (parameters.summOfCredit / totalPayment) * 100;
+        var percentsInPercent = 100 - mainDeptInPercent;
+        return new CalculationResult(payment, totalPayment, totalPercent, percentsInPercent);
+    }
+}
+
+internal record CalculationParameters(double summOfCredit, double personPeyYear, int periods);
+
+internal record CalculationResult(double payment, double totalPayment, double totalPercent, double percentsInPercent);
--- CreditCalculator/CreditCalculator/Program.cs
+++ CreditCalculator/CreditCalculator/Program.cs
@@ -1,17 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text;
+using CreditCalculator;
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.WriteLine("Credit calculator!");
 
-// X = c * k
+// X = summOfCredit * k
 // k коэффициент ануниента
 // с сумма кредита
 // x ежемесячный платеж
 // k = m *(1+m)^s/((1+m)^s -1)
 // m - месячная процентная ставка (если кредит под 24 годовых, то m = 2)
-// s - количество пратежей = кол-во месяцев если платим каждый месяц
+// periods - количество пратежей = кол-во месяцев если платим каждый месяц
 // 
 
 Console.WriteLine("какая сумма кредита?");
@@ -24,25 +25,17 @@ var personPeyYear = double.Parse(Console.ReadLine()!) / 100; // преобраз
 
 Console.WriteLine("какая месяцев выплаты кредита?");
 
-var s = int.Parse(Console.ReadLine()!);
+var periods = int.Parse(Console.ReadLine()!);
 
-var m = personPeyYear / 12;
-// коэффициент ануниента
-var k = m * Math.Pow(1 + m, s) / (Math.Pow(1 + m, s) - 1);
-// ежемесячный платеж
-var x = c * k;
+var calculator = new CreditCalculator.CreditCalculator();
+var (payment, totalPayment, totalPercent, totalPercentsInPercent) = calculator.Calc(new CalculationParameters(c, personPeyYear, periods));
 
-var payment = x;
-var totalPayment = x * s;
-var totalPercent = totalPayment - c;
 
-var mainDeptInPercent = (c / totalPayment) * 100;
-var percentsInPercent = 100 - mainDeptInPercent;
 
 Console.WriteLine($"ежемесячный платеж: {payment:C}");
 Console.WriteLine($"всего платежей: {totalPayment:C}");
 Console.WriteLine($"начисленные проценты: {totalPercent:C}");
-Console.WriteLine($"От общей уплаченой суммы. основной долг: {mainDeptInPercent:0.00}%; кол-во переплаченых процентов: {percentsInPercent:0.00}%");
+Console.WriteLine($"От общей уплаченой суммы. кол-во переплаченых процентов: {totalPercentsInPercent:0.00}%");
 
 
 var dept = c;
@@ -55,6 +48,7 @@ var sumMainDeptPayment = 0d;
 Console.WriteLine($"График платежа");
 Console.WriteLine($"№ \t Платеж\t\t тело креда\tпроценты\tостаток долга");
 var paymentInfoList = new List<PaymentInfo>();
+var m = personPeyYear / 12;
 while (dept > 0)
 {
     
@@ -72,7 +66,13 @@ while (dept > 0)
 
     dept -= mainDeptPayment;
 
-    var info = new PaymentInfo(paymentNumber, payment, Math.Round(mainDeptPayment, 2), Math.Round(percentPayment, 2), Math.Round(dept, 2));
+    var info = new PaymentInfo(
+        paymentNumber,
+        payment,
+        Math.Round(mainDeptPayment, 2),
+        Math.Round(percentPayment, 2),
+        Math.Round(dept, 2)
+    );
     Console.WriteLine(info);
     paymentInfoList.Add(info);
 
