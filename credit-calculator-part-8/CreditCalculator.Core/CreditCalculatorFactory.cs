82e5de4f1f792e292874f5aab820f6fc56a47f51
Kirill <kirill.0312@mail.ru>
2024-07-10 15:30:29 +0300

    clone from 7

---
+++ credit-calculator-part-8/CreditCalculator.Core/CreditCalculatorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace CreditCalculator.Core;
+
+public class CreditCalculatorFactory
+{
+    public ICreditCalculator CreateCalculator(
+        CalcType calcType
+    ) =>
+        calcType switch
+        {
+            CalcType.Annuity => new AnnuityCreditCalculator(),
+            CalcType.Differentiated => new DifferentiatedCreditCalculator(),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+}
