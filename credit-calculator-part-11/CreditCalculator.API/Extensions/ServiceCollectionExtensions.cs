ef4ded7e609d35b5ad24edffaf8111f85d8acb29
Kirill <kirill.0312@mail.ru>
2024-09-03 09:02:43 +0300

    create clone from 10 to 11

---
+++ credit-calculator-part-11/CreditCalculator.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace CreditCalculator.API.Extensions;
+
+using Configuration;
+using Core;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Services.CalculationHistory;
+
+public static class ServiceCollectionExtensions
+{
+    public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+
+        services.Configure<ServiceOptions>(configuration.GetRequiredSection(nameof(ServiceOptions)));
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policyBuilder =>
+            {
+                var corsOptions = builder.Configuration.GetRequiredSection(nameof(CorsOptions)).Get<CorsOptions>()!;
+                policyBuilder.WithOrigins(corsOptions.Origins).AllowAnyMethod().AllowAnyHeader();
+            });
+        });
+
+        services.AddScoped<CreditCalculatorFactory>();
+        services.AddScoped<ICalculationHistoryService, CalculationHistoryService>();
+        services.AddScoped<ICreditCalculationHistoryRepository, CreditCalculationHistoryRepository>();
+
+        services.AddDbContext<CreditCalculatorDbContext>(opt =>
+        {
+            opt
+                .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention();
+            //opt.UseInMemoryDatabase("CreditCalculator");
+            opt.LogTo(Console.WriteLine);
+        });
+
+        return builder;
+    }
+}
