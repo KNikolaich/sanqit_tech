8a2f5b99ccb1d463f161d9e5f7fc036caa8b78ae
k.fedorov <kirill.0312@mail.ru>
2025-04-05 15:17:06 +0300

    part12

---
+++ credit-calculator-part-12/CreditCalculator.API/Services/CalculationHistory/CalculationHistoryService.cs
@@ -0,0 +1,34 @@
+﻿namespace CreditCalculator.API.Services.CalculationHistory;
+
+using Configuration;
+using Infrastructure;
+using Microsoft.Extensions.Options;
+using Models;
+
+public interface ICalculationHistoryService
+{
+    IReadOnlyCollection<CalculationHistory> GetCalculationHistory();
+    void SaveCalculationHistory(CalculationHistory calculationHistory);
+}
+
+internal class CalculationHistoryService(
+    ICreditCalculationHistoryRepository calculationHistoryRepository,
+    IOptionsMonitor<ServiceOptions> optionsMonitor
+) : ICalculationHistoryService
+{
+    public IReadOnlyCollection<CalculationHistory> GetCalculationHistory() =>
+        optionsMonitor.CurrentValue.DisableCalculationHistory
+            ? []
+            : calculationHistoryRepository.GetCalculationHistory();
+
+    public void SaveCalculationHistory(CalculationHistory calculationHistory)
+    {
+        if (optionsMonitor.CurrentValue.DisableCalculationHistory)
+        {
+            return;
+        }
+
+        calculationHistory.CreatedOn = DateTime.UtcNow;
+        calculationHistoryRepository.AddHistory(calculationHistory);
+    }
+}
